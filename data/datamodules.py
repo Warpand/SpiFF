7b754953d502ea139d6bc7d8f070abd8d5a10206
brinpow <patryk.marszalek@student.uj.edu.pl>
2024-04-17 18:23:37 +0200

    Add ZincDataSet and ZincDataModule class

--- data/datamodules.py
+++ data/datamodules.py
@@ -1,5 +1,47 @@
+from pathlib import Path
+
 import pytorch_lightning
+import torch.utils.data
+from datasets import ZincDataset
+from featurizer import FeaturizerFactory
+from torch_geometric.loader import DataLoader
 
 
 class ZincDatamodule(pytorch_lightning.LightningDataModule):
-    pass
+    """
+    Data module class for ZincDataset.
+    """
+
+    def __init__(
+        self,
+        data_path: Path,
+        featurizer_factory: FeaturizerFactory,
+        batch_size: int,
+        num_workers: int = 8,
+    ) -> None:
+        """
+        Constructs ZincDataModule.
+
+        :param data_path: path to dataset file.
+        :param featurizer_factory: factory creating some featurizer for molecule data.
+        :param batch_size: batch size for Dataloader.
+        :param num_workers: number of subprocesses which are used in Dataloader.
+        """
+
+        super().__init__()
+
+        self.data = ZincDataset(data_path, featurizer_factory())
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+    def train_dataloader(self) -> torch.utils.data.DataLoader:
+        """
+        Returns train DataLoader for ZincDateset.
+        """
+
+        return DataLoader(
+            self.data,  # type: ignore
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            persistent_workers=True,
+        )
--- data/datasets.py
+++ data/datasets.py
@@ -1,5 +1,56 @@
+from pathlib import Path
+
+import pandas as pd
+import rdkit.Chem
+from featurizer import Featurizer, GraphFeatures
+from rdkit import Chem
 from torch.utils.data import Dataset
+from torch_geometric.data import Data
 
 
 class ZincDataset(Dataset):
-    pass
+    """
+    Dataset representing molecules from Zinc database.
+    """
+
+    def __init__(self, data_path: Path, featurizer: Featurizer) -> None:
+        """
+        Constructs ZincDataset.
+
+        :param data_path: path to dataset file.
+        :param featurizer: featurizer which can be used to
+        extract features from molecules.
+        """
+
+        super().__init__()
+        df = pd.read_csv(data_path, sep=" ")
+        self.molecules = [Chem.MolFromSmiles(smiles) for smiles in df["smiles"]]
+        self.feature_data = [featurizer.extract_features(mol) for mol in self.molecules]
+
+    def __getitem__(self, index: int) -> (GraphFeatures, int):
+        """
+        Returns features for molecule at given index and its index.
+
+        :param index: index of data sample.
+        """
+
+        data_sample = self.feature_data[index]
+        return Data(data_sample.node_features, data_sample.edge_index), index
+
+    def __len__(self) -> int:
+        """
+        Returns size of the dataset.
+
+        :return: size of the dataset.
+        """
+
+        return len(self.feature_data)
+
+    def get_molecule(self, index: int) -> rdkit.Chem.Mol:
+        """
+        Returns molecule at given index.
+
+        :param index: index of molecule.
+        """
+
+        return self.molecules[index]
